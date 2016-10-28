6c04a0d981b0b50ff76ae47dd6f5f7bf5e4e81c4
Satyan <msatyan@gmail.com>
2016-10-28 00:29:18 -0500

    Basic TS with HTML

--- app.ts
+++ app.ts
@@ -1,9 +1,37 @@
 // console.log("It works!");
-class Startup {
-    public static main(): number {
-        console.log('Hello World');
+
+
+class MySimpleTest
+{
+    private OutPutElem: any;
+
+    constructor(MyOutPutElem: any) 
+    {
+        this.OutPutElem = MyOutPutElem;
+        this.OutPutElem.innerText = "";
+    }
+
+    public  LogOutput(msg: string): void 
+    {
+        this.OutPutElem.innerText += msg;
+    }
+}
+
+class Startup 
+{
+    public static main(): number
+    {
+        console.log("Hello World");
+
+        var demo1 : MySimpleTest = new 
+        MySimpleTest( document.getElementById("OutPutElem") );
+
+        demo1.LogOutput("This is a");
+        demo1.LogOutput(" TypeScript Hello World! :-)");
+
         return 0;
     }
 }
 
-Startup.main();
+
+
